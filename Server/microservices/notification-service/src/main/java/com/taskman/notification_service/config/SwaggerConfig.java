d60f1e9b43d6cc7fda2d0e84a74e1f60f5e4d809
Hadari Sas <hadarisas@Hadaris-MacBook-Pro.local>
2024-12-10 23:58:00 +0100

    Modeling - Server Basic services

---
+++ Server/microservices/notification-service/src/main/java/com/taskman/notification_service/config/SwaggerConfig.java
@@ -0,0 +1,13 @@
+package com.taskman.notification_service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI CustomOpenApi (){
+        return new OpenAPI();
+    }
+}
